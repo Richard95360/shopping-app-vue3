c5dddce1490d440d76c8a9148f592553e0edb2e4
Richard95360 <richard.haddad1@free.fr>
2021-12-03 14:08:25 +0100

    first commit

---
+++ src/views/Product.vue
@@ -0,0 +1,21 @@
+<template>
+ <div>Mes Produits</div>
+</template>
+
+<script>
+
+export default {
+  name: 'Product',
+
+  methods: {
+   
+  },
+ 
+}
+</script>
+
+<style lang="scss">
+
+
+
+</style>
